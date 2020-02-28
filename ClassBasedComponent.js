450e06c37a376ea0bd53b1e2e70326331318c3bc
krishna kakade <krishnakakade77@gmail.com>
2020-02-28 19:42:21 +0530

    Create ClassBasedComponent.js

---
+++ ClassBasedComponent.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react"
+import ReactDOM from "react-dom"
+
+class App extends React.Component{
+
+    render(){
+
+      return(
+
+        <h1>This is my first ClassbasesComponent{this.props}</h1>
+
+            )
+           }
+         }
