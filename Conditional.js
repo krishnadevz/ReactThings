3538a95b6ffcd33420bfa42ef8a815069be31dcc
krishna kakade <krishnakakade77@gmail.com>
2020-03-08 16:04:16 +0530

    Condtional.js for rendering react condtnls

---
+++ Conditional.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Conditional(props) {
+   return (
+      /*ES6 ternery operators here->*/ props.isLoading===true? <h1>Losddsdfsdffdf</h1>:<h1>aosme ciooslissfdf</h1>
+   )
+}
+
+export default Conditional
