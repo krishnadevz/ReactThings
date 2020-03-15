740c8f60f98bfc11b252d3cf389e06350243db65
krishna kakade <krishnakakade77@gmail.com>
2020-03-15 19:53:37 +0530

    form in react

---
+++ form.js
@@ -0,0 +1,74 @@
+import React, {Component} from "react"
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: "",
+            lastName: "",
+            isFriendly: false
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(event) {
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+    }
+    
+    render() {
+        return (
+            <form>
+                <input 
+                    type="text" 
+                    value={this.state.firstName} 
+                    name="firstName" 
+                    placeholder="First Name" 
+                    onChange={this.handleChange} 
+                />
+                <br />
+                <input 
+                    type="text" 
+                    value={this.state.lastName} 
+                    name="lastName" 
+                    placeholder="Last Name" 
+                    onChange={this.handleChange} 
+                />
+                
+                {
+                    /**
+                     * Other useful form elements:
+                     * 
+                     *  <textarea /> element
+                     *  <input type="checkbox" />
+                     *  <input type="radio" />
+                     *  <select> and <option> elements
+                     */
+                }
+                
+                <textarea 
+                    value={"Some default value"}
+                    onChange={this.handleChange}
+                />
+                
+                <br />
+                
+                <label>
+                    <input 
+                        type="checkbox" 
+                        name="isFriendly"
+                        checked={this.state.isFriendly}
+                        onChange={this.handleChange}
+                    /> Is friendly?
+                </label>
+                
+                
+                
+                
+                <h1>{this.state.firstName} {this.state.lastName}</h1>
+            </form>
+        )
+    }
+}
+
+export default App
