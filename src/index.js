f9578fea3c2f005fd1e487ba1b243dac44b1d63c
krishna kakade <krishnakakade77@gmail.com>
2020-03-16 16:11:30 +0530

    Meme Generator App pushed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
-import { render } from '@testing-library/react';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDom.render(
-
-
-<ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-</ul>,
-document.getElementById("root")
-
-)
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
