e06d90871ff4381131c2e6bdfdc689fc66da918d
krishna kakade <krishnakakade77@gmail.com>
2020-03-16 16:27:22 +0530

    MemeGenerator App in react

---
+++ MemeGenerator/src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="https://m.media-amazon.com/images/M/MV5BYzllMjE1MTItZjQ5Mi00NTU2LWFiMTgtMGNkNzlhYzVkZmMzXkEyXkFqcGdeQXVyNjc5Mjg0NjU@._V1_QL50_.jpg" 
+                alt="Problem?"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
