a5eee97c3974c76cea21f5db22be3e239684088e
krishna kakade <krishnakakade77@gmail.com>
2020-03-17 10:44:12 +0530

    UseStatehook.jsx

---
+++ UseStatehook.jsx
@@ -0,0 +1,13 @@
+import react,{useStae} from "react"
+function App(){
+const [answer]=useState("yes lovely")
+return(
+
+  <div>
+  
+  <h1>Are you coming{answer}</h1>
+  </div>
+  
+)
+}
+//if anyone having something please drop comment we add or merge your thing thasall
